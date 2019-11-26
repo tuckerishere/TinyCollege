f6c8f9f14d1349b20b4c6598c032c8d564fc428e
tuckerishere <mtucker@DESKTOP-FK0P91M.localdomain>
2019-11-26 12:57:21 -0600

    Adding a student now adds ID

--- TinyCollege/AddStudent.cs
+++ TinyCollege/AddStudent.cs
@@ -30,14 +30,24 @@ namespace TinyCollege
 
         private void btnAddStudent_Click(object sender, EventArgs e)
         {
+            int count = 0;
+            using(conn = new SqlConnection(connectionString))
+            using(SqlCommand com = new SqlCommand
+                ("SELECT COUNT (*) FROM dbo.student", conn)){
+                conn.Open();
+                count = (int)com.ExecuteScalar();
+            };
+            
             using (conn = new SqlConnection(connectionString))
             using (SqlCommand comd = new SqlCommand
                 //added Indentity insert to allow the Student ID to be modified
                 ("SET IDENTITY_INSERT student ON INSERT INTO student (studentId, studentName) VALUES (@studentId, @studentName)", conn))
             {
+            
+
                 conn.Open();
                 //adds studentId to the table(Need to check if the student ID is in use and then add the next value)
-                comd.Parameters.AddWithValue("@studentId",3);
+                comd.Parameters.AddWithValue("@studentId", ++count);
                 //adds name entered in the text box field(need to create validation checks)
                 comd.Parameters.AddWithValue("@studentName", txtStudentName.Text);
                 comd.ExecuteScalar();
